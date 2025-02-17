8afd6a0ce614169fd1d09e9919955e303e0754ab
Marissa Loya <mloya1207@gmail.com>
2025-02-16 21:29:47 -0500

    completed Reddit ETL and pipeline. Begin working on AWS

--- dags/reddit_dag.py
+++ dags/reddit_dag.py
@@ -1,13 +1,16 @@
-from airflow import DAG
 from datetime import datetime
 import os
 import sys
-import PythonOperator
+
+from airflow import DAG
+from airflow.operators.python import PythonOperator
 
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
+from pipelines.reddit_pipeline import reddit_pipeline
+
 default_args = {
-    'owner': 'Marissa Loya'
+    'owner': 'Marissa Loya',
     'start_date': datetime(2025,2,15)
 }
 
@@ -24,12 +27,18 @@ dag = DAG(
 #extraction from reddit
 extract = PythonOperator(
     task_id = 'reddit_extraction',
-    python_callable = 'reddit_pipeline',
+    python_callable = reddit_pipeline,
     op_kwargs = {
         'file_name': f'reddit_{file_postfix}',
         'subreddit': 'dataengineering',
         'time_filter': 'day',
         'limit': 100
-    }
+    },
+    dag=dag
 )
 #upload to S3
+upload_s3 = PythonOperator(
+    task_id = 's3_upload',
+    python_callable = upload_s3_pipeline,
+    dag=dag
+)
